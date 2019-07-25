19d8512b527cb3358d0bc2108a5f41fb2c54ced5
Yvonne Nduta Wangui <v-yvndut@microsoft.com>
2019-07-25 10:03:38 +0300

    Adding Borrowed Resource and Office Item Model classes.

---
+++ msgraph-sharepoint-sample/Models/OfficeItem.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace msgraph_sharepoint_sample.Models
+{
+    class OfficeItem
+    {
+        [JsonIgnore]
+        public string SharePointItemId { get; set; }
+
+        [JsonProperty(PropertyName = "OfficeItemID")]
+
+        public Guid ItemId { get; set; }
+
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "Resource_x0020_IDLookupId")]
+        public string ResourceId { get; set; }
+
+        [JsonProperty(PropertyName = "SerialNo")]
+
+        public string SerialNumber { get; set; }
+
+        [JsonProperty(PropertyName = "Description")]
+        public string ItemDescription { get; set; }
+
+    }
+}
